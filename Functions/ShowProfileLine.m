0a182d4dfa6e6581de7612d1d1bd7a7b1d87aa8b
garghi2000 <garghi2000@hotmail.it>
2018-07-11 15:48:46 +0200

    ridistribution

---
+++ Functions/ShowProfileLine.m
@@ -0,0 +1,80 @@
+function ShowProfileLine(alldata)
+
+%-----------------------------------------------------------------------------------------
+%   Readme
+%   
+%   Hint: Before using this function set the custom properties of the
+%   images aspects(function SetImageDefaultProperties(alldata))
+%   
+% 
+%   This function does the following: 
+%   - Plots the data "alldata" using the matrix representation(origin on topleft corner);
+%   The aspect of the figure is defined by the function SetImageDefaultProperties()
+%   - Lets the user draw the line and drag or modify it after created.
+%   - Plots the line profile and upload it every time the drawn line is
+%   moved or modified
+%
+%                                                                           G.B. 03.07.2018
+%-----------------------------------------------------------------------------------------
+
+
+%% Main Part
+my_PlotData(alldata)%Plot the data with aspect properties given by SetImageDefaultProperties
+pos=my_GetPosProfileLine(alldata);%Draw the line; pos is the vector defining the position of the line
+my_PlotProfLine(pos);%Plot the profile line
+
+%% Functions used in this code
+
+%Plot Data
+function my_PlotData(alldata)
+figure('Name','Data','NumberTitle','off','Units','centimeters','pos', [5 5 20 20])
+imagesc(alldata);
+strtitle = sprintf('Left Click and drag to draw the line for a profile analysis');
+title(strtitle);
+%setproperties of the image
+my_SetImageDefaultProperties(alldata)
+end
+
+%Create the position of the inizial point and the final point of the line
+function pos = my_GetPosProfileLine(alldata)
+    %let user create a line
+    profline = imline();%matlab function to produce the line
+    strtitle = sprintf('Left clicking on the line to drag or modify it \n(the mouse icon changes)');
+    strtitle = compose(strtitle);
+    title(strtitle);%change title of the figure to help the user 
+    pos=profline.getPosition();%use the position of the line to create the profile
+    addNewPositionCallback(profline,@my_PlotProfLine)%callbacks the function @fnc when pos changes
+end
+
+%Use the output of GetProfileLine to plot the profile line of the image
+function my_PlotProfLine(pos)   
+    n = findobj('type','figure');
+    n = length(n);
+    if n <2 %create the figure for the profile line
+        figure('Name','Profile Line','NumberTitle','off','Units','centimeters','pos', [25 5 15 10]) 
+    else %if the figure already exists it change the current figure to overwrite the profile
+    PrlinFig =  findobj('type','figure','Name','Profile Line');
+    set(0,'CurrentFigure',PrlinFig);%makes the Profile line figure the current figure
+    end
+    xi =pos(:,1);
+    yi =pos(:,2);
+    h = improfile(alldata,xi,yi);%matlab function to profile line
+    plot(h);
+end
+
+%Set the properties of the current image
+function my_SetImageDefaultProperties(alldata)
+avg=nanmean(alldata(:));%mean
+sigma=nanstd(alldata(:));%standard deviation
+axis square;
+colormap(sxm.op.nanonisMap(128));% colormap defined by the nanonisMap NanoLib function
+caxis([avg-2*sigma avg+2*sigma])% Edge of the colormap
+ca=gca;
+ca.FontSize = 26;
+ca.TitleFontSizeMultiplier = 0.8;
+ca.LineWidth = 2;
+ca.YLim =  [0 size(alldata,1)];
+ca.XLim = [0 size(alldata,2)];
+end
+
+end
---
+++ Scripts/Code2GetResolution.m
@@ -0,0 +1,53 @@
+%-----------------------------------------------------------------------------------------
+%   Readme
+%   Hint: this script loads the sxm file with name defined by "loc_nfesem" vector and using
+%   the personal NanoLib library found in '..\..\matlab Library\NanoLib' path. 
+%   Before run the code be sure both file name and path of the library are
+%   correct.
+% 
+%   This script does the following:
+%   
+%  - Plots the raw data of the channel chosen using loadProcessedSxM. Note that in case of the
+%   channeltron channel all data were moltiplied by a -1, so that the all routine doen't have
+%   to be changed since the fit function is a down step function(it is negative).
+%  - Allows the user to select the region of interest(roi) inside the image loaded by clicking 
+%   and dragging the mouse. 
+%  - Fits to everyscan line of the roi the step function -A*tanh(B*(x-C)-D with A,B,C,D>0; 
+%   the fit parameter C is x coordinate of the inflection point of the tanh function
+%   i.e. the position of the step in a scanline. The y coordinate is the scan line index.
+%   All of these x,y coordinates define the points of the border line between W and C regions.
+%  - Saves the border line points in a txt file.
+%  - Plot the histogram of the contrast for every scan line using the ratio A/D of the fit
+%   coefficients. Note that this definition of contrast is equivalent to the one given by
+%   (W-C)/(W+C) where W and C are the mean values of the regions W and C.
+%  - Plot the histogram of the contrast for every scan line using (W-C)/(W+C) where W and C are
+%   the mean values respectively on the left and on the right region respect to the step 
+%   position on the masked image. 
+%   
+%  
+%  
+%
+%                                                       G.B. 08.06.2018
+%-----------------------------------------------------------------------------------------
+
+clear all;
+close all;
+
+%% path for file and NanoLib library
+clear all;
+close all;
+%path for file and NanoLib library
+addpath('..\..\matlab Library\NanoLib');
+loc='2017_02_02_2_W110_polished_file_003.sxm';
+
+%Load file with the NanoLib function loadProcessedSxM
+img=sxm.load.loadProcessedSxM(loc,'Mean');
+
+%Get the index of the channel named Z backward scan 
+Chbkw=utility.getChannel(img.channels,'Z','backward');
+
+%Store the only data in the matrix alldata
+alldata=img.channels(Chbkw).data.*1e12;%from meter to picometer
+
+%% get the resolution using 2 border files
+GetResolution(alldata);
